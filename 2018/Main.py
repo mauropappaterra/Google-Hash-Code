00339546adce4c23579082095fa1f6d8c0a96f42
yogurt1989 <mauropappaterra@gmail.com>
2018-03-02 14:47:45 +0100

    #hashcodesolved

--- 2018/Main.py
+++ 2018/Main.py
@@ -50,10 +50,10 @@ def reward (car, ride):
     return ride.points + bonus * B - max(ride.starting_time - current_time, distance_pickup_car) - validation
 
 # PATH TO INPUT
-path = "Input\\a_example.in"
+#path = "Input\\a_example.in"
 #path = "Input\\b_should_be_easy.in"
 #path = "Input\c_no_hurry.in"
-#path = "Input\d_metropolis.in"
+path = "Input\d_metropolis.in"
 #path = "Input\e_high_bonus.in"
 
 mode = "r"
